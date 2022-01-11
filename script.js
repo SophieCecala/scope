466095e0ed6ca4d5097660ad5e3c8a7e96ab9efa
Natalya Myunster <natalyamyunster@yahoo.com>
2022-01-11 17:41:34 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// task 4
+function func14() {
+    num14 = 2;
+}
+
+let num14 = 1;
+alert(num14); // 1
+alert(func14()); // nothing
+
+// task 15
+function func15() {
+    num15 = 2;
+}
+
+let num15 = 1;
+alert(func15());// nothing
+alert(num15); //2
